1c13e49fba44552541cece6b6ca63a025b0231da
xiaoming <867943335@qq.com>
2020-08-04 16:28:41 +0800

    新增协程使用例子

---
+++ Couroutine/src/main/java/com/xiaoming/couroutine/CoroutineExample.kt
@@ -0,0 +1,61 @@
+package com.xiaoming.couroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * author: xxm
+ * created on: 2020/8/4 16:10
+ * description:协程测试
+ */
+class CoroutineExample {
+
+    companion object {
+
+        @JvmStatic
+        fun main(arg: Array<String>) {
+//            testGlobalScopeLaunch()
+
+//            testRunBlocking()
+            runBlocking {
+//                launch {
+//                    testJob()
+//                }
+                testJob()
+            }
+        }
+
+        private fun testGlobalScopeLaunch() {
+            GlobalScope.launch {
+                delay(1000L)
+                println("World！")
+            }
+            println("Hello，")
+
+            //阻塞主线程
+            runBlocking {
+                delay(1200L)
+            }
+        }
+
+        private fun testRunBlocking() = runBlocking {
+            GlobalScope.launch {
+                delay(1000L)
+                println("World！")
+            }
+            println("Hello，")
+            delay(1200L)
+        }
+
+        private suspend fun testJob() {
+            val job = GlobalScope.launch {
+                delay(1000L)
+                println("World！")
+            }
+            println("Hello，")
+            job.join()//等待直到子线程结束
+        }
+    }
+}
---
+++ Couroutine/src/main/java/com/xiaoming/couroutine/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.xiaoming.couroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
